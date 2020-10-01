b1c2a2d7005f7d7e02a2e2cdc35f6fd11eeb6801
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2020-10-02 01:45:14 +0300

    2020-04-02

---
+++ src/main/scala/c2020_04/w1/d02.scala
@@ -0,0 +1,32 @@
+package c2020_04.w1
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+
+class d02 extends AnyWordSpec with Matchers {
+
+  object Solution {
+    def isHappy(n: Int): Boolean = {
+      import scala.annotation.tailrec
+
+      @tailrec
+      def loop(i: Int, acc: Set[Int]): Boolean =
+        if (i == 1) true
+        else if (acc.contains(i)) false
+        else loop(i.toString.foldLeft(0)((r, c) => r + Math.pow(c.asDigit, 2).toInt), acc + i)
+
+      loop(n, Set())
+    }
+  }
+
+  "Solution" should {
+    "19" in {
+      Solution.isHappy(19) shouldBe true
+    }
+
+    "2" in {
+      Solution.isHappy(2) shouldBe false
+    }
+  }
+}
