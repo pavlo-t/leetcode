5938166f36e9152a54519e8963e1494549fa8374
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2020-11-09 19:27:16 +0200

    Weekly Contest 214

--- src/main/scala/contest/w214/w214_4.scala
+++ src/main/scala/contest/w214/w214_4.scala
@@ -8,13 +8,148 @@ class w214_4 extends AnyWordSpec with Matchers {
 
   /**
    * <h3>5564. Create Sorted Array through Instructions</h3>
+   *
+   * Given an integer array `instructions`, you are asked to create a sorted array from the elements in `instructions`.
+   * You start with an empty container `nums`.
+   * For each element from <b>left to right</b> in `instructions`, insert it into `nums`.
+   * The <b>cost</b> of each insertion is the <b>minimum</b> of the following:<ul>
+   * <li> The number of elements currently in `nums` that are <b>strictly less than</b> `instructions[i]`.
+   * <li> The number of elements currently in `nums` that are <b>strictly greater than</b> `instructions[i]`.
+   * </ul>
+   *
+   * <p>For example, if inserting element `3` into `nums = [1,2,3,5]`, the <b>cost</b> of insertion is `min(2, 1)`
+   * (elements `1` and `2` are less than `3`, element `5` is greater than `3`) and `nums` will become `[1,2,3,3,5]`.
+   *
+   * <p>Return <em>the <b>total cost</b> to insert all elements from `instructions` into `nums`</em>.
+   * Since the answer may be large, return it <b>modulo</b> `1_000_000_007`
+   *
+   * <p><b>Constraints:</b><ul>
+   * <li> `1 <= instructions.length <= 100_000`
+   * <li> `1 <= instructions[i] <= 100_000`
+   * </ul>
    */
   object Solution {
     val Modulo = 1_000_000_007
     def createSortedArray(instructions: Array[Int]): Int = {
+      instructions
+        .zipWithIndex
+        .foldLeft((0, new BinaryIndexedTree(100_000))) { case ((acc, bit), (v, i)) =>
+          ((acc + (bit.sum(v - 1) min (i - bit.sum(v)))) % Modulo, bit.add(v))
+        }._1
+    }
+
+    /**
+     * Binary Indexed Tree aka Fenwick Tree
+     *
+     * @see [[https://en.wikipedia.org/wiki/Fenwick_tree]]
+     * @see [[https://www.hackerearth.com/practice/notes/binary-indexed-tree-or-fenwick-tree/]]
+     */
+    class BinaryIndexedTree(size: Int) {
+      private val A = Array.ofDim[Int](size + 1)
+      private def lsb(i: Int): Int = i & -i
+
+      @scala.annotation.tailrec
+      final def sum(i: Int, rsf: Int = 0): Int =
+        if (i > 0) sum(i - lsb(i), rsf + A(i))
+        else rsf
+
+      @scala.annotation.tailrec
+      final def add(i: Int): BinaryIndexedTree =
+        if (i <= size) {
+          A(i) += 1
+          add(i + lsb(i))
+        } else this
+    }
+  }
+  object SolutionMyBIT {
+    val Modulo = 1_000_000_007
+    def createSortedArray(instructions: Array[Int]): Int = {
+      def lsb(i: Int): Int = i & -i
+
+      val size = 100_000
+      val bit = Array.ofDim[Int](size + 1)
+
+      @scala.annotation.tailrec
+      def sum(i: Int, rsf: Int = 0): Int =
+        if (i > 0) sum(i - lsb(i), rsf + bit(i))
+        else rsf
+
+      @scala.annotation.tailrec
+      def add(i: Int): Unit =
+        if (i <= size) {
+          bit(i) += 1
+          add(i + lsb(i))
+        }
+
+      instructions
+        .indices
+        .fold(0) { (acc, i) =>
+          val v = instructions(i)
+          val newRes = (acc + (sum(v - 1) min (i - sum(v)))) % Modulo
+          add(v)
+          newRes
+        }
+    }
+  }
+  /**
+   * [[https://leetcode.com/problems/create-sorted-array-through-instructions/discuss/927531/JavaC%2B%2BPython-Binary-Indexed-Tree]]
+   */
+  object Solution1 {
+    val Modulo = 1_000_000_007
+    def createSortedArray(instructions: Array[Int]): Int = {
+      val max = 100_000
+      val c = Array.ofDim[Int](max + 1)
+
+      def update(x: Int): Unit = {
+        var i = x
+        while (i <= max) {
+          c(i) += 1
+          i += i & -i
+        }
+      }
+
+      def get(x: Int): Int = {
+        var i = x
+        var res = 0
+        while (i > 0) {
+          res += c(i)
+          i -= i & -i
+        }
+        res
+      }
+
+      var res = 0
+      for (i <- instructions.indices) {
+        res += Math.min(get(instructions(i) - 1), i - get(instructions(i)))
+        res %= Modulo
+        update(instructions(i))
+      }
+      res
+    }
+
+  }
+  object SolutionMyWithCache {
+    val Modulo = 1_000_000_007
+    def createSortedArray(instructions: Array[Int]): Int = {
       var cost = 0
-      //val cache = collection.mutable.Map[Int, Int]()
-      for (i <- (1 until instructions.length)) {
+      val cache = collection.mutable.Map[Int, Int](instructions.head -> 1).withDefaultValue(0)
+      for (i <- instructions.tail) {
+        val (costL, costR) = cache.foldLeft((0, 0)) { case (acc@(costL, costR), k -> count) =>
+          if (k < i) (costL + count, costR)
+          else if (k > i) (costL, costR + count)
+          else acc
+        }
+        cost += (costL min costR) % Modulo
+        cache(i) += 1
+      }
+      cost
+    }
+  }
+  object SolutionMyBruteForce {
+    val Modulo = 1_000_000_007
+    def createSortedArray(instructions: Array[Int]): Int = {
+      var cost = 0
+      for (i <- 1 until instructions.length) {
         var costL = 0
         var costR = 0
         for (j <- i to 0 by -1) {
@@ -64,7 +199,14 @@ class w214_4 extends AnyWordSpec with Matchers {
     //The total cost is 0 + 0 + 0 + 0 + 1 + 0 + 1 + 0 + 2 = 4.
   }
 
-  "(instructions = [random of length 100_000]) -> >= 0" in {
+  "(instructions = [1]) -> 0" in {
+    createSortedArray(Array(1)) shouldBe 0
+  }
+  "(instructions = [1 repeat 100_000]) -> 0" in {
+    val instructions = Array.fill(100_000)(1)
+    createSortedArray(instructions) shouldBe 0
+  }
+  "(instructions = [random repeat 100_000]) -> >= 0" in {
     val instructions = Array.fill(100_000)(util.Random.nextInt(100_000) + 1)
     createSortedArray(instructions) should be >= 0
   }
