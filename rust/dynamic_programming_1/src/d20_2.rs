3bb6e233685d6f61d7eee1e792cab3a624d96b3f
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2021-10-27 19:54:44 +0300

    Dynamic Programming 1: Day 20

--- rust/dynamic_programming_1/src/d20_2.rs
+++ rust/dynamic_programming_1/src/d20_2.rs
@@ -24,7 +24,7 @@ struct Solution;
 impl Solution {
     /// Approach 1: Dynamic Programming
     /// https://leetcode.com/problems/coin-change-2/solution/
-    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
+    pub fn change_leetcode_dp(amount: i32, coins: Vec<i32>) -> i32 {
         println!("change({}, {:?})", amount, coins);
         let amount = amount as usize;
         let mut dp = vec![0; amount + 1];
@@ -36,25 +36,42 @@ impl Solution {
         }
         dp[amount]
     }
-    /// 23:04-23:14
-    pub fn change_dp_2_vec_vec(amount: i32, coins: Vec<i32>) -> i32 {
+
+    /// 19:47-19:50
+    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
         println!("change({}, {:?})", amount, coins);
+        let coins = coins.into_iter().map(|c| c as usize).collect::<Vec<_>>();
         let amount = amount as usize;
-        let coins = coins.into_iter().map(|i| i as usize).collect::<Vec<_>>();
-        let mut dp = vec![vec![0; amount + 1]; coins.len() + 1];
+        let mut dp = vec![0; amount + 1];
+        dp[0] = 1;
         for i in (0..coins.len()).rev() {
-            dp[i][amount] = 1;
-            for a in (0..amount).rev() {
-                for j in i..coins.len() {
-                    if a + coins[j] <= amount {
-                        dp[i][a] += dp[j][a + coins[j]];
-                    }
-                }
+            for a in coins[i]..=amount {
+                dp[a] += dp[a - coins[i]];
             }
         }
-        dp[0][0]
+        dp[amount]
     }
-    /// 23:01-23:04
+    /// 19:39-19:47
+    pub fn change_dp(amount: i32, coins: Vec<i32>) -> i32 {
+        println!("change({}, {:?})", amount, coins);
+        let coins = coins.into_iter().map(|c| c as usize).collect::<Vec<_>>();
+        let amount = amount as usize;
+        let mut dp = vec![vec![1; amount + 1]; coins.len() + 1];
+        for a in 1..=amount {
+            dp[coins.len()][a] = 0;
+        }
+        for i in (0..coins.len()).rev() {
+            for a in 1..=amount {
+                dp[i][a] = if coins[i] <= a {
+                    dp[i + 1][a] + dp[i][a - coins[i]]
+                } else {
+                    dp[i + 1][a]
+                };
+            }
+        }
+        dp[0][amount]
+    }
+    /// 19:35-19:39
     pub fn change_rec_2_with_memo(amount: i32, coins: Vec<i32>) -> i32 {
         println!("change({}, {:?})", amount, coins);
         fn rec(i: usize, a: usize, cs: &[usize], memo: &mut Vec<Vec<i32>>) -> i32 {
@@ -65,22 +82,20 @@ impl Solution {
             } else if memo[i][a] >= 0 {
                 memo[i][a]
             } else {
-                let mut result = 0;
-                for j in i..cs.len() {
-                    if cs[j] <= a {
-                        result += rec(j, a - cs[j], cs, memo);
-                    }
-                }
-                memo[i][a] = result;
+                memo[i][a] = if cs[i] <= a {
+                    rec(i + 1, a, cs, memo) + rec(i, a - cs[i], cs, memo)
+                } else {
+                    rec(i + 1, a, cs, memo)
+                };
                 memo[i][a]
             }
         }
+        let coins = coins.into_iter().map(|c| c as usize).collect::<Vec<_>>();
         let amount = amount as usize;
-        let coins = coins.into_iter().map(|i| i as usize).collect::<Vec<_>>();
         let mut memo = vec![vec![-1; amount + 1]; coins.len()];
         rec(0, amount, &coins, &mut memo)
     }
-    /// 22:57-23:01
+    /// 18:41-19:35
     pub fn change_rec_2(amount: i32, coins: Vec<i32>) -> i32 {
         println!("change({}, {:?})", amount, coins);
         fn rec(i: usize, a: usize, cs: &[usize]) -> i32 {
@@ -88,26 +103,21 @@ impl Solution {
                 1
             } else if i == cs.len() {
                 0
+            } else if cs[i] <= a {
+                rec(i + 1, a, cs) + rec(i, a - cs[i], cs)
             } else {
-                let mut result = 0;
-                for j in i..cs.len() {
-                    if cs[j] <= a {
-                        result += rec(j, a - cs[j], cs);
-                    }
-                }
-                result
+                rec(i + 1, a, cs)
             }
         }
-        let coins = coins.into_iter().map(|i| i as usize).collect::<Vec<_>>();
-        rec(0, amount as usize, &coins)
+        let coins = coins.into_iter().map(|c| c as usize).collect::<Vec<_>>();
+        let amount = amount as usize;
+        rec(0, amount, &coins)
     }
+
     /// 22:02-22:30
     pub fn change_dp_1_vec_vec(amount: i32, coins: Vec<i32>) -> i32 {
         println!("change({}, {:?})", amount, coins);
-        let coins = coins
-            .into_iter()
-            .map(|c| c as usize)
-            .collect::<Vec<_>>();
+        let coins = coins.into_iter().map(|c| c as usize).collect::<Vec<_>>();
         let amount = amount as usize;
         let mut dp = vec![vec![0; coins.len()]; amount + 1];
         for i in 0..coins.len() {
@@ -178,7 +188,6 @@ mod tests {
     use super::*;
 
     #[rustfmt::skip] #[test] fn a_10_c_1() { assert_eq!(Solution::change(10, vec![1]), 1); }
-    #[rustfmt::skip] #[test] fn a_0_c_1_2() { assert_eq!(Solution::change(0, vec![1, 2]), 1); }
     #[test]
     fn a_5_c_1_2_5() {
         assert_eq!(Solution::change(5, vec![1, 2, 5]), 4);
@@ -189,14 +198,48 @@ mod tests {
         // 5=1+1+1+1+1
     }
     #[test]
-    fn a_3_c_2() {
-        assert_eq!(Solution::change(3, vec![2]), 0);
-        // Explanation: the amount of 3 cannot be made up just with coins of 2.
-    }
-    #[test]
     fn a_10_c_10() {
         assert_eq!(Solution::change(10, vec![10]), 1);
     }
+    #[rustfmt::skip] #[test] fn a_0_c_1_2()   { assert_eq!(Solution::change(0, vec![1, 2   ]), 1); }
+    #[rustfmt::skip] #[test] fn a_0_c_1_2_3() { assert_eq!(Solution::change(0, vec![1, 2, 3]), 1); }
+
+    #[rustfmt::skip] #[test] fn a_1_c_1()     { assert_eq!(Solution::change(1, vec![1      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_1_c_2()     { assert_eq!(Solution::change(1, vec![2      ]), 0); }
+    #[rustfmt::skip] #[test] fn a_1_c_1_2()   { assert_eq!(Solution::change(1, vec![1, 2   ]), 1); } // 1
+    #[rustfmt::skip] #[test] fn a_1_c_1_2_3() { assert_eq!(Solution::change(1, vec![1, 2, 3]), 1); }
+
+    #[rustfmt::skip] #[test] fn a_2_c_1()     { assert_eq!(Solution::change(2, vec![1      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_2_c_2()     { assert_eq!(Solution::change(2, vec![2      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_2_c_1_2()   { assert_eq!(Solution::change(2, vec![1, 2   ]), 2); } // 11 2
+    #[rustfmt::skip] #[test] fn a_2_c_1_2_3() { assert_eq!(Solution::change(2, vec![1, 2, 3]), 2); } // 11 2
+
+    #[rustfmt::skip] #[test] fn a_3_c_1()     { assert_eq!(Solution::change(3, vec![1      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_3_c_2()     { assert_eq!(Solution::change(3, vec![2      ]), 0); }
+    #[rustfmt::skip] #[test] fn a_3_c_1_2()   { assert_eq!(Solution::change(3, vec![1, 2   ]), 2); } // 111 12
+    #[rustfmt::skip] #[test] fn a_3_c_1_2_3() { assert_eq!(Solution::change(3, vec![1, 2, 3]), 3); } // 111 12 3
+
+    #[rustfmt::skip] #[test] fn a_4_c_1()     { assert_eq!(Solution::change(4, vec![1      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_4_c_2()     { assert_eq!(Solution::change(4, vec![2      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_4_c_1_2()   { assert_eq!(Solution::change(4, vec![1, 2   ]), 3); } // 1111 112 22
+    #[rustfmt::skip] #[test] fn a_4_c_1_2_3() { assert_eq!(Solution::change(4, vec![1, 2, 3]), 4); } // 1111 112 13 22
+
+    #[rustfmt::skip] #[test] fn a_5_c_1()     { assert_eq!(Solution::change(5, vec![1      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_5_c_2()     { assert_eq!(Solution::change(5, vec![2      ]), 0); }
+    #[rustfmt::skip] #[test] fn a_5_c_1_2()   { assert_eq!(Solution::change(5, vec![1, 2   ]), 3); } // 11111 1112 122
+    #[rustfmt::skip] #[test] fn a_5_c_1_2_3() { assert_eq!(Solution::change(5, vec![1, 2, 3]), 5); } // 11111 1112 113 122 23
+
+    #[rustfmt::skip] #[test] fn a_6_c_1()     { assert_eq!(Solution::change(6, vec![1      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_6_c_2()     { assert_eq!(Solution::change(6, vec![2      ]), 1); }
+    #[rustfmt::skip] #[test] fn a_6_c_1_2()   { assert_eq!(Solution::change(6, vec![1, 2   ]), 4); } // 111111 11112 1122 222
+    #[rustfmt::skip] #[test] fn a_6_c_1_2_3() { assert_eq!(Solution::change(6, vec![1, 2, 3]), 7); }
+    // c a 0 1 2 3 4 5 6
+    // 1 | 1 1 1 1 1 1 1
+    // 2 | 1 1 2 2 3 3 4
+    // 3 | 1 1 2 3 4 5 7
+    //
+    // dp[ci][amount] = dp[ci - 1][amount] + if coins[ci] <= amount { dp[ci][amount - coins[ci] } else { 0 }
+    // rec(i + 1, amount) + if cs[i] <= amount { rec(i, amount - cs[i]) } else { 0 }
 
     #[test]
     fn a_5000_c_1() {
